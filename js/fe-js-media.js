0c39cac56acb795198b24e14a82364fb4cef258d
melaniemdm <melanieborja.pro@gmail.com>
2021-05-12 11:32:21 +0200

    feature (js): creat media & photographes js

---
+++ js/fe-js-media.js
@@ -0,0 +1,13 @@
+function media(id, photographerId, title, image,tags,likes, date, price){
+    this.id = id;
+    this.photographerId = photographerId;
+    this.title = title;
+    this.image = image;
+    this.tags = tags;
+    this.likes = likes;
+    this.date = date;
+    this.price = price;
+   };
+  
+  var media0 = new media (342550,82,"Fashion Yellow Beach","Fashion_Yellow_Beach.jpg",["fashion"],62,"2011-12-08",55);
+  
---
+++ js/fe-js-photographes.js
@@ -0,0 +1,19 @@
+
+function photographers(name, id, city, country, tags,tagline, price,portrait ){
+    this.name = name;
+    this.id = id;
+    this.city = city;
+    this.country = country;
+    this.tags = tags;
+    this.tagline = tagline;
+    this.price = price;
+    this.portrait = portrait
+  };
+  
+  var photographers0 = new photographers("Mimi Keel", 243, "London", "UK",["portrait", "events", "travel", "animals"],"Voir le beau dans le quotidien", 400, "MimiKeel.jpg");
+  var photographers1 = new photographers("Ellie-Rose Wilkens",930,"Paris","France",["sports", "architecture"],"Capturer des compositions complexes",250,"EllieRoseWilkens.jpg");
+  var photographers2 = new photographers("Tracy Galindo",82,"Montreal","Canada",["art", "fashion", "events"],"Photographe freelance",500, "TracyGalindo.jpg");
+  var photographers3 = new photographers("Nabeel Bradford",527,"Mexico City","Mexico",["travel", "portrait"],"Toujours aller de l'avant",350,"NabeelBradford.jpg");
+  var photographers4 = new photographers("Rhode Dubois",925,"Barcelona","Spain",["sport", "fashion", "events", "animals"],"Je crée des souvenirs",275,"RhodeDubois.jpg");
+  var photographers5 = new photographers("Marcel Nikolic",195,"Berlin","Germany",["travel", "architecture"],"Toujours à la recherche de LA photo",300,"MarcelNikolic.jpg");
+ 
