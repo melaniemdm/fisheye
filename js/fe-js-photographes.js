e55b1f8ba86a78841dc46d3382de4aef0eca6734
melaniemdm <melanieborja.pro@gmail.com>
2021-05-18 10:11:44 +0200

    refacto(js &html): nommage

--- js/fe-js-photographes.js
+++ js/fe-js-photographes.js
@@ -4,34 +4,33 @@ async function getJSON(url, numero){
   let rep = await fetch(url, { method: 'GET' });
   let reponse = await rep.json();
  
-let toto = '';
-toto = toto + '<div class="card_photographe"> '
-toto = toto + '           <div class="photo_photographe">' + '<img src= " photos/sample%20Photos/Photographers%20ID%20Photos/' + reponse["photographers"][numero].portrait + '"/>' 
-             
-toto = toto + '           </a> '
-toto = toto + '            </div> '
-toto = toto + '           <h2 id="nom"> ' + reponse["photographers"][numero].name+ '</h2> '
+let presentationPhotographes = '';
+presentationPhotographes = presentationPhotographes + '<div class="card_photographe"> '
+presentationPhotographes = presentationPhotographes + '<div class="photo_photographe">' + '<img src= " photos/sample%20Photos/Photographers%20ID%20Photos/' + reponse["photographers"][numero].portrait + '"/>' 
+presentationPhotographes = presentationPhotographes + '</a> '
+presentationPhotographes = presentationPhotographes + '</div> '
+presentationPhotographes = presentationPhotographes + '<h2 id="nom"> ' + reponse["photographers"][numero].name+ '</h2> '
 
-toto = toto + '          <div class="info_photographe"> '
-toto = toto + '          <div id="ville" >' + reponse["photographers"][numero].city + '</div> '
-toto = toto + '          <div id="country">'+reponse["photographers"][numero].country + '</div>'
-toto = toto + '          <div id="tagline">' + reponse["photographers"][numero].tagline + '</div> '
-toto = toto + '           <div id="price">' + reponse["photographers"][numero].price + " €/jour" + '</div> '
-toto = toto + '<div class ="listeTags">'
+presentationPhotographes = presentationPhotographes + '<div class="info_photographe"> '
+presentationPhotographes = presentationPhotographes + '<div class="ville" >' + reponse["photographers"][numero].city + '</div> '
+presentationPhotographes = presentationPhotographes + '<div class="country">'+reponse["photographers"][numero].country + '</div>'
+presentationPhotographes = presentationPhotographes + '<div class="tagline">' + reponse["photographers"][numero].tagline + '</div> '
+presentationPhotographes = presentationPhotographes + '<div class="price">' + reponse["photographers"][numero].price + " €/jour" + '</div> '
+presentationPhotographes = presentationPhotographes + '<div class ="listeTags">'
 for(j = 0; j<reponse["photographers"][numero]. tags.length;j++){
-  toto = toto + ' <div class="tags">' + "#" + reponse["photographers"][numero].tags [j]+ ' </div>' 
+  presentationPhotographes = presentationPhotographes + ' <div class="tags">' + "#" + reponse["photographers"][numero].tags [j]+ ' </div>' 
 }
- toto= toto + '</div>'
-toto = toto + '           </div> '
-toto = toto + '       </div> '
+presentationPhotographes= presentationPhotographes + '</div>'
+presentationPhotographes = presentationPhotographes + '</div> '
+presentationPhotographes = presentationPhotographes + '</div> '
 
-document.querySelector("#toto").innerHTML = document.querySelector("#toto").innerHTML  + toto; 
+document.querySelector("#presentationPhotographes").innerHTML = document.querySelector("#presentationPhotographes").innerHTML  + presentationPhotographes; 
 
   return reponse;
 }
    
 
-
+//Boucle pour afficher tous les photographes
 for(i=0;i<6;i++){
   getJSON('http://127.0.0.1:5500/json/FishEyeData.json', i)
 }
