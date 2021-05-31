74294baf641f247a9f9f5608928d18b8e7606043
melaniemdm <melanieborja.pro@gmail.com>
2021-05-31 14:07:43 +0200

    fix(html): fonction ajoutMedia & photographes fix(js) : constructor pour les videos fix(js): fonction trie feature(js): choix du trie & event change

--- js/fe-js-gestion-des-medias-des-photographes.js
+++ js/fe-js-gestion-des-medias-des-photographes.js
@@ -1,182 +1,164 @@
 // creation des types de classes specifiques aux medias
-class Photo{
-  constructor(){
-    this._type = "photo"
-       }
+class Photo {
+  constructor() {
+    this._type = "photo";
+  }
 }
-class Courtmetrage{
-  constructor(){
-    this._type = "courtmetrage"
-       }
+class Courtmetrage {
+  constructor() {
+    this._type = "courtmetrage";
+  }
 }
 
 //création factory method qui permet de fabriquer le composant générique
-class MediaFactory{
+class MediaFactory {
   //permet de creer la fabrique des medias
-  constructor(){
+  constructor() {
     //fonction avec les paramètre de la création d'un media
-    this.createMedia= function(type, medias, title, like, heart){
-      let media
+    this.createMedia = function (type, image, video, title, like, heart) {
+      let media;
 
       //conditions qui permetent de gerer la création des medias specifiques
       if (type == "courtmetrage") {
-        media = new Courtmetrage()
+        media = new Courtmetrage();
+        var baliseMedia = ` <video controls width="250">
+        <source src="` 
+        var fermetureBaliseMedia = `"</source></video>`
+         media.medias = video;
       }
       if (type == "photo") {
-      media = new Photo()
-       }
+        media = new Photo();
+        var baliseMedia = `<img src="`
+        var fermetureBaliseMedia = `">`
+        media.medias = image;
+      }
 
-       //ajout des caracteristiques des medias
-      media.medias = medias;
+      //ajout des caracteristiques des medias
+     
       media.title = title;
       media.like = like;
-      
 
-
-//fonction qui crait le composant média
-      media.creatComposant = function(){
-      var composantElementMedia= `<div class="contenairMedia">
-<div class="photoIdPhotographes"><img src="` + media.medias +`"></div>
-<div class="title"> `+ media.title + `</div>
-<div class="likes"> `+ media.like + `</div>
+      //fonction qui crait le composant média
+      media.creatComposant = function () {
+        var composantElementMedia =
+          `<div class="contenairMedia">
+<div class="photoIdPhotographes">` + baliseMedia +
+          media.medias +
+          fermetureBaliseMedia + `</div>
+<div class="title"> ` +
+          media.title +
+          `</div>
+<div class="likes"> ` +
+          media.like +
+          `</div>
 <div class="heart"> <i class="fas fa-heart"></i></div>
-</div>`
+</div>`;
 
-    return composantElementMedia
-      }
-      return media
-    }
+        return composantElementMedia;
+      };
+      return media;
+    };
   }
 }
-
-//variable qui permet de creer une nouvelle fabrique - instance de fabrique
-let lesMedias = new MediaFactory()
 
 
 //fonction qui ajoute le composant et le contenu dans la page html
-async function ajoutComposantDansMediasPhotographes(idPhotographes){
-var listeMedias = document.querySelector("#listeDesMediasDuPhotographe")
-var composantMedia = await creationComposantMediaPhotographe (idPhotographes)
-for(i=0;i<composantMedia.length;i++){
-listeMedias.innerHTML = listeMedias.innerHTML + composantMedia[i]
-}
-}
+function ajoutComposantDansMediasPhotographes(media) {
+  var listeMedias = document.querySelector("#listeDesMediasDuPhotographe");
+  var composantMedia =  creationComposantMediaPhotographe(media)
+ 
+    listeMedias.innerHTML = listeMedias.innerHTML + composantMedia;
+  }
 
 //fonction qui permet de creer le composant html
-async function creationComposantMediaPhotographe (idPhotographes){
-var elementDeLaPhoto = await ajoutMediaDuPhotographe(idPhotographes)
-var compteur =0
-var listeDesComposants = new Array()
-for(i=0;i<elementDeLaPhoto.length;i++){
-  var composantElementMedia= elementDeLaPhoto[i].creatComposant()
-  listeDesComposants[compteur]= composantElementMedia
-  compteur++
-}
-return listeDesComposants
+function creationComposantMediaPhotographe(media) {
+   var composantElementMedia = media.creatComposant()
+        return composantElementMedia;
 }
 // fonction qui recupere le json
-async function recupereJsonMedia(){
-  let url = 'http://127.0.0.1:5501/json/FishEyeData.json'
-  let rep = await fetch(url, { method: 'GET' });
-let reponse = await rep.json();
-var tabloMedias = reponse["media"]
-console.log(tabloMedias)
-var tabloMediaTrie = await sortByProperty(tabloMedias, "title", 1) // tri par titre
-return tabloMediaTrie
+async function recupereJsonMedia(trie) {
+  let url = "http://127.0.0.1:5501/json/FishEyeData.json";
+  let rep = await fetch(url, { method: "GET" });
+  let reponse = await rep.json();
+  var tabloMedias = reponse["media"];
+
+ 
+
+  var tabloMediaTrie = await sortByProperty(tabloMedias, trie, 1); // appel de la fonction tri
+  return tabloMediaTrie;
 }
+
+ var trieMedia = document.querySelector("#filtre")
+  trieMedia.addEventListener("change", change_valeur )
+//fonction qui recupere la valeur du trie
+  function change_valeur() {
+console.log(trieMedia.value)
+document.querySelector("#listeDesMediasDuPhotographe").innerHTML = ""
+ajoutMediaDuPhotographe(idPhotographe, trieMedia.value)
+    }
+  
+
 
 //fonction qui recupere l'élément média, permet de creer le media
-async function recuperationElementMedia(idMedia){
-// stock le retour de la fonction dans la variable 
-var tabloMedias =  await recupereJsonMedia()
-//Boucle pour afficher tous les medias
-for(i=0;i<tabloMedias.length;i++){
-  console.log(tabloMedias[i].id) // recupere les id dans la console
-  if(tabloMedias[i].id== idMedia){
-var tableauElementsMedia = lesMedias.createMedia("photo","photos/sample%20Photos/Tracy/"+ tabloMedias[i].image,  tabloMedias[i].title, tabloMedias[i].likes)
+function recuperationElementMedia(numMedia) {
+  let lesMedias = new MediaFactory();
+  let nomSplit = document.querySelector(".nomDuPhotographe").innerHTML.split(" ")[0]
+  var choixMedia 
+  if(numMedia.image){
+choixMedia = "photo"
+  }else{
+choixMedia = "courtmetrage"
   }
-}
- return tableauElementsMedia
-}
-// fonction qui affiche tou sles medias
-async function ajoutMediaDuPhotographe(idPhotographes){
-  var tabloMedias =  await recupereJsonMedia()
-  var listeDesMedias = new Array()
-var compteur =0
-  for(i=0;i<tabloMedias.length;i++){
-    if(tabloMedias[i].photographerId== idPhotographes){
-      var recuptNomPhotographe = document.querySelector(".nomDuPhotographe")
-    var resultatNom =   recuptNomPhotographe.innerHTML
+  return lesMedias.createMedia(
+  // stock le retour de la fonction dans la variable
+          choixMedia,
+        "photos/sample%20Photos/" + nomSplit + "/" + 
+        numMedia.image,
+        "photos/sample%20Photos/" + nomSplit + "/" + 
+        numMedia.video,
+        numMedia.title,
+        numMedia.likes
+      )
 
-var nomSplit = resultatNom.split(" ")[0]
-console.log( nomSplit)
-      var tableauElementsMedia = lesMedias.createMedia("photo","photos/sample%20Photos/"+ nomSplit +"/"+ tabloMedias[i].image,  tabloMedias[i].title, tabloMedias[i].likes)
-      listeDesMedias[compteur]= tableauElementsMedia
-      compteur++
-        }
-      }
-  return listeDesMedias
-}
-//fonction de tri 
-async function sortByProperty(objArray, prop, direction){
-  if (arguments.length<2) throw new Error("ARRAY, AND OBJECT PROPERTY MINIMUM ARGUMENTS, OPTIONAL DIRECTION");
+    }
+
+
+// fonction qui affiche tous les medias
+async function ajoutMediaDuPhotographe(id, trie) {
+  var tabloMedias = await recupereJsonMedia(trie);
+  console.log('here')
+  //boucle
+  for (i = 0; i < tabloMedias.length; i++) {
+    var media =     recuperationElementMedia(tabloMedias[i])    
+    if (tabloMedias[i].photographerId == id){
+    ajoutComposantDansMediasPhotographes(media)
+  }}
+ }
+
+
+//fonction de tri
+async function sortByProperty(objArray, prop, direction) {
+  if (arguments.length < 2)
+    throw new Error(
+      "ARRAY, AND OBJECT PROPERTY MINIMUM ARGUMENTS, OPTIONAL DIRECTION"
+    );
   if (!Array.isArray(objArray)) throw new Error("FIRST ARGUMENT NOT AN ARRAY");
   const clone = objArray.slice(0);
-  const direct = arguments.length>2 ? arguments[2] : 1; //Default to ascending
-  const propPath = (prop.constructor===Array) ? prop : prop.split(".");
-  clone.sort(function(a,b){
-      for (let p in propPath){
-              if (a[propPath[p]] && b[propPath[p]]){
-                  a = a[propPath[p]];
-                  b = b[propPath[p]];
-              }
+  const direct = arguments.length > 2 ? arguments[2] : 1; //Default to ascending
+  const propPath = prop.constructor === Array ? prop : prop.split(".");
+  clone.sort(function (a, b) {
+    for (let p in propPath) {
+      if (a[propPath[p]] && b[propPath[p]]) {
+        a = a[propPath[p]];
+        b = b[propPath[p]];
       }
-      //convert numeric strings to integers
-      a = a.match(/^\d+$/) ? +a : a;
-      b = b.match(/^\d+$/) ? +b : b;
-      return ( (a < b) ? -1*direct : ((a > b) ? 1*direct : 0) );
+    }
+    //converti les string en nombre
+    if(isNaN(a)){
+    a = a.match(/^\d+$/) ? +a : a;
+    b = b.match(/^\d+$/) ? +b : b;
+  }
+    return a < b ? -1 * direct : a > b ? 1 * direct : 0;
   });
   return clone;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- js/fe-js-gestion-des-photographes.js
+++ js/fe-js-gestion-des-photographes.js
@@ -1,124 +1,150 @@
 // creation des types de classes specifiques aux artistes (dans le cas ou ils y auraient d'autres artistes autres que des photographes)
-class Photographe{
-  constructor(){
-    this._type = "photographe"
-       }
+class Photographe {
+  constructor() {
+    this._type = "photographe";
+  }
 }
 //création factory method qui permet de fabriquer le composant générique
-class ArtistesFactory{
-   //permet de creer la fabrique des Artistes
-  constructor(){
+class ArtistesFactory {
+  //permet de creer la fabrique des Artistes
+  constructor() {
     //fonction avec les paramètre de la création d'un artiste
-    this.createArtistes = function(type, photo, id, nom, city, country, tagline, price, tags ){
-let artiste
-//condition qui permet de gerer la création des artistes specifiques
-if (type == "photographe") {
-  artiste = new Photographe ()     
+    this.createArtistes = function (
+      type,
+      photo,
+      id,
+      nom,
+      city,
+      country,
+      tagline,
+      price,
+      tags
+    ) {
+      let artiste;
+      //condition qui permet de gerer la création des artistes specifiques
+      if (type == "photographe") {
+        artiste = new Photographe();
+      }
+
+      //ajout des caracteristiques des artistes
+      artiste.photo = photo;
+      artiste.id = id;
+      artiste.nom = nom;
+      artiste.city = city;
+      artiste.country = country;
+      artiste.tagline = tagline;
+      artiste.price = price;
+      artiste.tag = tags;
+
+      //fonction qui creait le composant Artiste
+      artiste.createComposant = function () {
+        var strComposant =
+          `<div class="contenairPhotographe">
+  <a href="medias-des-photographe.html?idPhotographe=` +
+          artiste.id +
+          `">
+  <div class="photoIdDuPhotographe">
+      <img src=" ` +
+          artiste.photo +
+          `">
+  </div>
+  <div class="nomDuPhotographe">` +
+          artiste.nom +
+          ` </div>
+  <div class="cityDuPhotographe">` +
+          artiste.city +
+          ` </div> <div class="countryDuPhotographes">` +
+          artiste.country +
+          ` </div>
+  <div class="taglineDuPhotographe"> ` +
+          artiste.tagline +
+          `</div>
+  <div class="priceDuPhotographe">` +
+          artiste.price +
+          `</div>`;
+        ('<div class ="tagsDuPhotographe">');
+        //  boucle qui permet de mettre le #
+        artiste.tag.forEach((element) => {
+          strComposant =
+            strComposant + ' <div class="tags">' + "#" + element + " </div>";
+        });
+        strComposant = strComposant + `</a>` + `</div>`;
+
+        return strComposant;
+      };
+      return artiste;
+    };
+  }
 }
 
-//ajout des caracteristiques des artistes
-artiste.photo =photo;
-artiste.id =id;
-artiste.nom =nom;
-artiste.city =city;
-artiste.country =country;
-artiste.tagline =tagline;
-artiste.price =price;
-artiste.tag =tags;
 
-
-//fonction qui creait le composant Artiste
-artiste.createComposant=function(){
-  var strComposant = 
-  `<div class="contenairPhotographe">
-  <a href="medias-des-photographe.html?idPhotographe=` + artiste.id + `">
-  <div class="photoIdDuPhotographe">
-      <img src=" `+ artiste.photo + `">
-  </div>
-  <div class="nomDuPhotographe">`+ artiste.nom +` </div>
-  <div class="cityDuPhotographe">`+ artiste.city +` </div> <div class="countryDuPhotographes">`+artiste.country +` </div>
-  <div class="taglineDuPhotographe"> `+ artiste.tagline +`</div>
-  <div class="priceDuPhotographe">`+ artiste.price + `</div>`
-     '<div class ="tagsDuPhotographe">'
-//  boucle qui permet de mettre le #
-  artiste.tag.forEach(element => {
-    strComposant = strComposant+ ' <div class="tags">' + "#" + element + ' </div>'
-       });
-  strComposant = strComposant + `</a>`+ `</div>`
-    
-
-  return strComposant
-
-  }
-return artiste
-}}}
-
-//variable qui permet de creer une nouvelle fabrique - instance de fabrique
-let lesPhotographes = new ArtistesFactory()
 //fonction qui ajoute le composant et le contenu dans la page html
-async function ajoutComposantDansIndex(idPhotographe){
-var listePhotographes = document.querySelector("#listeDesPhotographes")
-var composant = await creationComposantPhotographe(idPhotographe)
-listePhotographes.innerHTML = listePhotographes.innerHTML + composant
+function ajoutComposantDansIndex(idPhotographe) {
+  var listePhotographes = document.querySelector("#listeDesPhotographes");
+  var composant = creationComposantPhotographe(idPhotographe);
+  listePhotographes.innerHTML = listePhotographes.innerHTML + composant;
 }
 
 //fonction qui permet de creer le composant html
-async function creationComposantPhotographe (idPhotographe){
-var elementsDuPhotographe = await recuperationDonneesPhotographe(idPhotographe);
-var composantPhotographe = elementsDuPhotographe.createComposant()
- return composantPhotographe
+function creationComposantPhotographe(idPhotographe) {
+  var elementsDuPhotographe =  recuperationDonneesPhotographe(
+    idPhotographe
+  );
+  var composantPhotographe = elementsDuPhotographe.createComposant();
+  return composantPhotographe;
 }
 // fonction qui recupere le json
-async function recupereJson(){
-  let url = 'http://127.0.0.1:5501/json/FishEyeData.json'
-  let rep = await fetch(url, { method: 'GET' });
-let reponse = await rep.json();
-var tabloPhotographes = reponse["photographers"]
-console.log(tabloPhotographes.length)
-return tabloPhotographes
+async function recupereJson() {
+  let url = "http://127.0.0.1:5501/json/FishEyeData.json";
+  let rep = await fetch(url, { method: "GET" });
+  let reponse = await rep.json();
+  var tabloPhotographes = reponse["photographers"];
+  console.log(tabloPhotographes.length);
+  return tabloPhotographes;
 }
 
 //fonction qui recupere les éléments de l'artiste
-async function recuperationDonneesPhotographe(idPhotographe){
- // stock le retour de la fonction dans la variable 
-var tabloPhotographes =  await recupereJson()
+function recuperationDonneesPhotographe(photographe) {
+  const factoryArtiste = new ArtistesFactory();
+  return factoryArtiste.createArtistes(
+    "photographe",
+        "photos/sample%20Photos/Photographers%20ID%20Photos/" +
+        photographe.portrait,
+        photographe.id,
+        photographe.name,
+        photographe.city,
+        photographe.country,
+        photographe.tagline,
+        photographe.price + "€/jour",
+        photographe.tags
+  )
 
-//Boucle pour afficher tous les photographes
-for(i=0;i<tabloPhotographes.length;i++){
-  console.log(tabloPhotographes[i].id)
-  if(tabloPhotographes[i].id== idPhotographe){
-var tableauElementsPhotographes = lesPhotographes.createArtistes("photographe","photos/sample%20Photos/Photographers%20ID%20Photos/"+ tabloPhotographes[i].portrait, tabloPhotographes[i].id, tabloPhotographes[i].name,tabloPhotographes[i].city, tabloPhotographes[i].country, tabloPhotographes[i].tagline,tabloPhotographes[i].price + "€/jour",tabloPhotographes[i].tags )
-  }
-}
-return tableauElementsPhotographes 
-}
-
+    }
+  
 
 // fonction qui permet d'afficher tous les photographes
 async function ajoutTousLesPhotographes (){
   var tabloPhotographes =  await recupereJson()
+  console.log(tabloPhotographes);
 //Boucle pour afficher tous les photographes
 for(i=0;i<tabloPhotographes.length;i++){
-  ajoutComposantDansIndex(tabloPhotographes[i].id)
-}}
+  ajoutComposantDansIndex(tabloPhotographes[i])
+}
 
+}
 
+//fonction affiche un seul photographe
+async function ajoutUnSeulPhotographe(id){
+  var tabloPhotographes =  await recupereJson()
+  console.log(tabloPhotographes);
+//Boucle pour afficher tous les photographes
+for(i=0;i<tabloPhotographes.length;i++){
+  if(tabloPhotographes[i].id == id){
+  ajoutComposantDansIndex(tabloPhotographes[i])
+}
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
