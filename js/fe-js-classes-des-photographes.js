c6ce45454f9002f2b4e504be8c045d9ecf55e1b2
melaniemdm <melanieborja.pro@gmail.com>
2021-06-03 12:22:01 +0200

    refacto(js): création des fichiers classes

---
+++ js/fe-js-classes-des-photographes.js
@@ -0,0 +1,78 @@
+// creation des types de classes specifiques aux artistes (dans le cas ou ils y auraient d'autres artistes autres que des photographes)
+class Photographe {
+    constructor() {
+      this._type = "photographe";
+    }
+  }
+  //création factory method qui permet de fabriquer le composant générique
+  class ArtistesFactory {
+    //permet de creer la fabrique des Artistes
+    constructor() {
+      //fonction avec les paramètre de la création d'un artiste
+      this.creatArtistes = function (
+        type,
+        photo,
+        id,
+        nom,
+        city,
+        country,
+        tagline,
+        price,
+        tags
+      ) {
+        let artiste;
+        //condition qui permet de gerer la création des artistes specifiques
+        if (type == "photographe") {
+          artiste = new Photographe();
+        }
+  
+        //ajout des caracteristiques des artistes
+        artiste.photo = photo;
+        artiste.id = id;
+        artiste.nom = nom;
+        artiste.city = city;
+        artiste.country = country;
+        artiste.tagline = tagline;
+        artiste.price = price;
+        artiste.tag = tags;
+  
+        //fonction qui creait le composant Artiste
+        artiste.createComposant = function () {
+          var strComposant =
+            `<div class="contenairPhotographe">
+    <a href="medias-des-photographe.html?idPhotographe=` +
+            artiste.id +
+            `">
+    <div class="photoIdDuPhotographe">
+        <img src=" ` +
+            artiste.photo +
+            `">
+    </div>
+    <div class="nomDuPhotographe">` +
+            artiste.nom +
+            ` </div>
+    <div class="cityDuPhotographe">` +
+            artiste.city +
+            ` </div> <div class="countryDuPhotographes">` +
+            artiste.country +
+            ` </div>
+    <div class="taglineDuPhotographe"> ` +
+            artiste.tagline +
+            `</div>
+    <div class="priceDuPhotographe">` +
+            artiste.price +
+            `</div>`;
+          ('<div class ="tagsDuPhotographe">');
+          //  boucle qui permet de mettre le #
+          artiste.tag.forEach((element) => {
+            strComposant =
+              strComposant + ' <div class="tags">' + "#" + element + " </div>";
+          });
+          strComposant = strComposant + `</a>` + `</div>`;
+  
+          return strComposant;
+        };
+        return artiste;
+      };
+    }
+  }
