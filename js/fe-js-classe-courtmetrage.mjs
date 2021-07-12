4ad37142dd7d723e8c2b7a61a982e4b2e8323c27
melaniemdm <melanieborja.pro@gmail.com>
2021-07-12 14:20:15 +0200

    refacto(js): class: image & video commentaires

--- js/fe-js-classe-courtmetrage.mjs
+++ js/fe-js-classe-courtmetrage.mjs
@@ -1,8 +1,9 @@
+/* class Courtmetrage pour les medias des photographes au format video */
+/*---ajoute le fichier video dans la variable media---*/
 class Courtmetrage {
     constructor(video) {
         this._type = "courtmetrage";
         this.medias = video;
     }
 }
-
 export { Courtmetrage };
--- js/fe-js-classe-des-photos.mjs
+++ js/fe-js-classe-des-photos.mjs
@@ -1,10 +1,9 @@
-// creation des types de classes specifiques aux medias
+/* class Photo pour les medias des photographes au format image */
+/*---ajoute le fichier image dans la variable media---*/
 class Photo {
     constructor(image) {
         this._type = "photo";
         this.medias = image;
     }
 }
-  
-  
 export { Photo };
