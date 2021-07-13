a9f21892ec376c15b96e4105b4a1b514ce08d164
melaniemdm <melanieborja.pro@gmail.com>
2021-07-13 13:29:55 +0200

    refacto(js) gestion des photographes

--- js/fe-js-gestion-des-photographes.js
+++ js/fe-js-gestion-des-photographes.js
@@ -1,67 +1,58 @@
 import { Photographe } from './fe-js-classe-des-photographes.js';
-
-// fonction qui recupere le json
+/*Affiche tous les photographes*/
+export async function displayAllPhotographers (){
+    //recupere les photographes dans le json
+    var arrayPhotographersFromJson = await getPhotographersFromJson();
+    //boucle
+    for (let i = 0; i < arrayPhotographersFromJson .length; i++) { 
+        //recupere l'objet photographe  créé grâce à la class
+        var objetPhotographe = await getPhotographerObjectFromClass(arrayPhotographersFromJson[i],i);
+        //recupere le noeudf de la liste des photographes
+        var nodePhotographersList = document.querySelector("#listeDesPhotographes");
+        //créer le composdant html du photographe et l'ajoute a la page html
+        nodePhotographersList.innerHTML = nodePhotographersList.innerHTML + objetPhotographe.createComposant();
+    }
+}
+/* fonction qui recupere le json*/
 async function getPhotographersFromJson() {
     let url = "https://melaniemdm.github.io/fisheye/json/FishEyeData.json";
     let rep = await fetch(url, { method: "GET" });
     let reponse = await rep.json();
     let arrayPhotographers = reponse["photographers"];
-
     return arrayPhotographers;}
 
-//Affiche tous les photographes
-export async function displayAllPhotographers (){
-    var arrayPhotographersFromJson = await getPhotographersFromJson();
-    console.log("etape1:", arrayPhotographersFromJson);
-    for (let i = 0; i < arrayPhotographersFromJson .length; i++) { 
-        var objetPhotographe = await getPhotographerObjectFromClass(arrayPhotographersFromJson[i],i);
-        console.log("etape2:", objetPhotographe);
-        var nodePhotographersList = document.querySelector("#listeDesPhotographes");
-        nodePhotographersList.innerHTML = nodePhotographersList.innerHTML + objetPhotographe.createComposant();
-        console.log("etape3:", objetPhotographe.createComposant());
-    }
-}
+/*fonction qui créé un objet photographe fr$ace aux données d'un photographe dans le json*/
+async function getPhotographerObjectFromClass(photographe, index){
+    //utilisation de new pour la création d'un nouveau photographe
+    var lesPhotographes = new Photographe("photographe", "photos/Sample_Photos/Photographers_ID_Photos/" + photographe.portrait, photographe.id, photographe.name, photographe.city, photographe.country, photographe.tagline, photographe.price+ "€/jour", photographe.tags, index);
+    return lesPhotographes;}
 
-//fonction affiche UN photographe dans la page Media
+/*fonction qui affiche UN photographe dans la page Media*/
 export async function displayOnePhotographer (id){
-    
+    //recupere les photrographes dans le json
     var arrayPhotographersFromJson = await getPhotographersFromJson();
-   
     for (let i = 0; i < arrayPhotographersFromJson .length; i++) { 
         if (arrayPhotographersFromJson[i].id === parseInt(id)){
             var nodePhotographersList = document.querySelector("#lePhotographe");
-            
-            //demande a recuperer l'object grace a la factory avec les infos recuperé dans le json
+            //demande a recuperer l'object grace a la class des photographes avec les infos recuperé dans le json
             var objetPhotographe = await getPhotographerObjectFromClass(arrayPhotographersFromJson[i],i);
-           
             nodePhotographersList.innerHTML = nodePhotographersList.innerHTML + objetPhotographe.createComposantLarge();
         } 
     }
- 
-    
     return 0;
 }
 
-
-//fonction affiche les photographes en fonction du tag
+/*fonction qui affiche les photographes en fonction du tag*/
 export async function displayAllPhotographersTagues (tag){
-  
     var arrayPhotographersFromJson = await getPhotographersFromJson();
     var nodePhotographersList = document.querySelector("#listeDesPhotographes");
     nodePhotographersList.innerHTML = ""; // pour vider et recréer
     for (let i = 0; i < arrayPhotographersFromJson .length; i++) { 
-   
         //verifie si le tableau contient une sous chaine de caractere, renvoie true s'il existe
         if (arrayPhotographersFromJson[i].tags.includes(tag) === true){
+            //demande a recuperer l'object grace a la class des photographes avec les infos recuperé dans le json
             var objetPhotographe = await getPhotographerObjectFromClass(arrayPhotographersFromJson[i],i);
             nodePhotographersList.innerHTML = nodePhotographersList.innerHTML + objetPhotographe.createComposant();
         }
     }}
-
-//fonction recupere les elements
-async function getPhotographerObjectFromClass(photographe, index){
- 
-    var lesPhotographes = new Photographe("photographe", "photos/Sample_Photos/Photographers_ID_Photos/" + photographe.portrait, photographe.id, photographe.name, photographe.city, photographe.country, photographe.tagline, photographe.price+ "€/jour", photographe.tags, index);
-    
-    return lesPhotographes;}
 
