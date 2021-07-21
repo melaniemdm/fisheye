74f906220607d13e6c036b77e7e8ac82571af4ad
melaniemdm <melanieborja.pro@gmail.com>
2021-07-21 22:08:46 +0200

    refacto(js): commentaire

--- js/fe-js-navigation.js
+++ js/fe-js-navigation.js
@@ -1,25 +1,25 @@
 
+// event scroll
 document.addEventListener("scroll",displayBtnContenu);
-//btn navigation
+//btn contenu 
 function displayBtnContenu(){
     let btnContenu = document.querySelector(".boutonContenu");
     if(btnContenu){
-        if(window.pageYOffset > 50) {
+        if(window.pageYOffset > 50) { // regarde le niveau où je suis ds la page
             btnContenu.style.visibility = 'visible';
         }else{
             btnContenu.style.visibility = 'hidden';
         }}
 }
 /* navigation au clavier*/
-
 window.addEventListener("keydown", function(event) {
     if(!window.isModalOpened){
-        if (event.keyCode === 13) { /* espace pour clicker*/
+        if (event.keyCode === 13) { /* entrée pour clicker*/
             event.preventDefault(); /* empecher le comportement par défaut*/
             event.target.click();
             event.target.firstChild.click();    
         }
-        if (event.keyCode === 8) {
+        if (event.keyCode === 8) { // retour arrière - action précédent
             window.history.back();
         }}
     
