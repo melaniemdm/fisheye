1993530be0da2f49dc51210c0f611eb38b8d9984
melaniemdm <melanieborja.pro@gmail.com>
2021-07-22 14:15:20 +0200

    refacto(js): mise en forme html

--- js/fe-js-classe-des-photographes.js
+++ js/fe-js-classe-des-photographes.js
@@ -16,31 +16,30 @@ class Photographe {
     /*fonction qui permet de créer le composant html a afficher sous forme de chaine de caractere */
     getComposantHtml() {
         var strComposant =
-    `<div class="contenairPhotographe" >
-        <a href="medias-des-photographe.html?idPhotographe=` + this.id + `" tabindex="-1">
-            <div class="photoIdDuPhotographe" tabindex="` + this.index + `1">
-                <img src=" ` + this.photo +`" alt="`+this.nom+`">
-                <h2 class="nomDuPhotographe">` + this.nom + ` </h2>    
-            </div>
-            <div class="infoDuPhotographe" tabindex="` + this.index + `2">
-                <div class="localisation"> 
-                    <div class="cityDuPhotographe">` +  this.city + ",&nbsp; "+ ` </div> <div class="countryDuPhotographes">` + this.country + ` </div>
-                </div>
-                <div class="taglineDuPhotographe"> ` + this.tagline + `</div>
-                <div class="priceDuPhotographe">` + this.price + `</div>
-            </div></a>`;
-        
+            `<div class="contenairPhotographe" >
+                <a href="medias-des-photographe.html?idPhotographe=` + this.id + `" tabindex="-1">
+                    <div class="photoIdDuPhotographe" tabindex="` + this.index + `1">
+                        <img src=" ` + this.photo +`" alt="`+this.nom+`">
+                        <h2 class="nomDuPhotographe">` + this.nom + ` </h2>    
+                    </div>
+                    <div class="infoDuPhotographe" tabindex="` + this.index + `2">
+                        <div class="localisation"> 
+                            <div class="cityDuPhotographe">` +  this.city + ",&nbsp; "+ ` </div> <div class="countryDuPhotographes">` + this.country + ` </div>
+                        </div>
+                        <div class="taglineDuPhotographe"> ` + this.tagline + `</div>
+                        <div class="priceDuPhotographe">` + this.price + `</div>
+                    </div></a>`;
+                
         strComposant =strComposant +
-            '<div class=""><nav class ="tagsDuPhotographe" tabindex="' + this.index + '3">';
+                    '<div class=""><nav class ="tagsDuPhotographe" tabindex="' + this.index + '3">';
         //  boucle qui permet de mettre le #
         this.tag.forEach((element) => {
             strComposant = strComposant + 
-            `    <div class="tags"><span role="button" onclick="displayAllPhotographersTagues('`+element +`')" aria-label="tag  `  + element +'">'  + "#" + element + " </span></div>";
+                     `<div class="tags"><span role="button" onclick="displayAllPhotographersTagues('`+element +`')" aria-label="tag  `  + element +'">'  + "#" + element + " </span></div>";
         });
         strComposant = strComposant +
-            `</nav> </div>  
-        ` + 
-    `</div>`;
+                    `</nav> </div> 
+            ` + `</div>`;
         /* retourne la chaine de caractere créée  */
         return strComposant;
     }
@@ -48,19 +47,19 @@ class Photographe {
     getComposantHtmlLarge() {
         var strComposant =
     `
-<div class="contenairPhotographeLarge">
-<div class="photoIdDuPhotographeLarge" tabindex="8">
-    <img src=" ` + this.photo + `" alt="`+this.nom+`">
-</div>
-<div class="contact" tabindex="7"> 
-<div role="button" aria-label="contact me" class="btn-contact" id="btn-contact"> Contactez-moi </div>
-</div>
-<div class="infoDuPhotographeLarge" >
-<h1 class="nomDuPhotographeLarge" tabindex="0">` + this.nom + `  </h1>
-<div class="localisationLarge" tabindex="1  ">        
-<div class="cityDuPhotographeLarge" >` + this.city + ",&nbsp "+ ` </div> <div class="countryDuPhotographesLarge">` + this.country +` </div> 
-</div>
-<div class="taglineDuPhotographeLarge"> ` + this.tagline +  `</div>`;
+            <div class="contenairPhotographeLarge">
+                <div class="photoIdDuPhotographeLarge" tabindex="8">
+                    <img src=" ` + this.photo + `" alt="`+this.nom+`">
+                </div>
+                <div class="contact" tabindex="7"> 
+                    <div role="button" aria-label="contact me" class="btn-contact" id="btn-contact"> Contactez-moi </div>
+                </div>
+                <div class="infoDuPhotographeLarge" >
+                    <h1 class="nomDuPhotographeLarge" tabindex="0">` + this.nom + `  </h1>
+                    <div class="localisationLarge" tabindex="1  ">        
+                        <div class="cityDuPhotographeLarge" >` + this.city + ",&nbsp "+ ` </div> <div class="countryDuPhotographesLarge">` + this.country +` </div> 
+                        </div>
+                    <div class="taglineDuPhotographeLarge"> ` + this.tagline +  `</div>`;
         strComposant =strComposant +'<div class ="tagsDuPhotographeLarge">';
         //  boucle qui permet de mettre le #
         let tagIndex = 3;
@@ -68,13 +67,13 @@ class Photographe {
             strComposant =  strComposant + `<div onclick="launchDisplayPhotographersbytags('`+ element +`')" role="button" class="tags" tabindex="`+ tagIndex +'">'  + "#" + element + " </div>";
             tagIndex++;
         });
-        strComposant = strComposant +`</div>  
-</div> 
-</div> 
-<div id="likePriceFooter">
-<div class="likePriceFooter"> <span id="totalLikesPage"> 0 </span> <span  aria-label="likes"class="material-icons-outlined title="likes_">favorite
-</span> `+this.price +` </div></div>
-    </a> `;
+        strComposant = strComposant +`
+                        </div>  
+                    </div> 
+                </div> 
+                <div id="likePriceFooter">
+                    <div class="likePriceFooter"> <span id="totalLikesPage"> 0 </span> <span  aria-label="likes"class="material-icons-outlined title="likes_">favorite
+                    </span> `+this.price +` </div></div>`;
         return strComposant;
     }
 }
