53a3ef3cd926ae22783f554d5aa461368c4ab70e
melaniemdm <melanieborja.pro@gmail.com>
2021-08-02 00:19:14 +0200

    feature(js classePhoto): ajout class galleryItem     getComposantHtml() feature(js gallery): createComposantGallery & keycodes feature(css): modif style gallery feature(html medias) ajout lien css gallery

--- js/fe-js-classe-des-photos.mjs
+++ js/fe-js-classe-des-photos.mjs
@@ -9,19 +9,19 @@ class Photo {
     getComposantHtml() {
         let composantElementMedia;
         composantElementMedia =
-  ` 
+` 
 <a href="` +this.url+`galeryGrdeTaille/` + this.medias + `" onclick="return false;" class="light-link" data-sub-html=" <h4 class='titleInGallery'>`+ this.title + ` - `+ this.price+`€</h4>"> 
-  <img src="`+ this.url + this.medias + `" alt="`+ this.alt_text+  `"/>
-  <div class="infoContenair">
-    <div class="titleMedias"> `+ this.title+ `</div>
-    <div class="likes">
-      <div aria-label="nombre de likes" id="likes_`+ this.id +`"> ` +   this.like + `</div>
-      <div class="heart" role="button" aria-label="ajouter un like"> 
-        <span  aria-label="likes" class="material-icons-outlined" title="likes_`+ this.id + `">favorite
-        </span>
-      </div>
+<img src="`+ this.url + this.medias + `" alt="`+ this.alt_text+  `" class="galleryItem"/>
+<div class="infoContenair">
+  <div class="titleMedias"> `+ this.title+ `</div>
+  <div class="likes">
+    <div aria-label="nombre de likes" id="likes_`+ this.id +`"> ` +   this.like + `</div>
+    <div class="heart" role="button" aria-label="ajouter un like"> 
+      <span  aria-label="likes" class="material-icons-outlined" title="likes_`+ this.id + `">favorite
+      </span>
     </div>
   </div>
+</div>
 </a> 
 `;
         return composantElementMedia;
--- js/fe-js-gallery.js
+++ js/fe-js-gallery.js
@@ -1,3 +1,66 @@
 export function chargeGallery(noeudHTML){
-    noeudHTML.innerHTML = "wahou";
+    let composantGalleryHTML = `
+        <div id="gallery">
+            <div id="media">
+            </div>
+            <div id="miniature">
+            </div>
+            <div id="closeCross"><span class="material-icons-outlined">
+            close
+            </span></div>
+            <div id="leftArrow"><span class="material-icons-outlined">
+            chevron_left
+            </span></div>
+            <div id="rightArrow"><span class="material-icons-outlined">
+            chevron_right
+            </span></div>            
+        </div>`;
+
+    noeudHTML.innerHTML =  noeudHTML.innerHTML + composantGalleryHTML;
+  
+}
+
+document.addEventListener("click",openGallery);
+
+function openGallery(e){
+    window.addEventListener("keydown",navigationGallery);
+    console.log(e.target);
+    if(e.target.className === "galleryItem"){
+        document.querySelector("#gallery").style.visibility = "visible";
+        document.querySelector("#media").innerHTML = `<img src="` + e.target.parentNode.href + `"/>`;
+    }
+    if (e.target.id === "gallery" || e.target.parentNode.id === "closeCross"){
+        closeGallery();
+    }
+    if(e.target.parentNode.id === "leftArrow"){
+        goLeft();
+    }
+    if(e.target.parentNode.id === "rightArrow"){
+        goRight();
+    }
+}
+
+function navigationGallery(event){
+    if(event.keyCode === 37){
+        goLeft();
+    }
+    if(event.keyCode === 39){
+        goRight();
+    }
+    if(event.keyCode === 27){
+        closeGallery();
+    }       
+}
+
+function goLeft(){
+    alert("image précédente");
+}
+
+function goRight(){
+    alert("image suivante");
+}
+
+function closeGallery(){
+    document.querySelector("#gallery").style.visibility = "hidden";
+    window.removeEventListener("keydown",navigationGallery);
 }
